ae313062c52bbaacca5621441c8be8a0435b0360
tegarswasono <tegar.s@weefer.co.id>
2024-01-29 10:32:51 +0700

    add basic getting started

--- LearnQuestPDF/Controllers/WeatherForecastController.cs
+++ LearnQuestPDF/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
 
 namespace LearnQuestPDF.Controllers
 {
@@ -21,6 +25,57 @@ namespace LearnQuestPDF.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            //https://www.questpdf.com/quick-start.html
+            QuestPDF.Settings.License = LicenseType.Community;
+
+            QuickStart();
+            var lineItems = new List<LineItem>()
+            {
+                new() { Index = 0, Name = "TShirt Child", Price = "10" },
+                new() { Index = 1, Name = "TShirt Man", Price = "20" },
+                new() { Index = 2, Name = "TShirt Women", Price = "30" }
+            };
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Margin(50);
+                    page.Size(PageSizes.A4);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(16));
+
+                    page.Header()
+                        .AlignCenter()
+                        .Text("Invoice #: 2023-77")
+                        .SemiBold().FontSize(24).FontColor(Colors.Grey.Darken4);
+
+                    page.Content()
+                        .Table(table =>
+                        {
+                            table.ColumnsDefinition(columns =>
+                            {
+                                columns.ConstantColumn(20);
+                                columns.RelativeColumn();
+                                columns.RelativeColumn();
+                            });
+
+                            table.Header(header =>
+                            {
+                                header.Cell().Text("#");
+                                header.Cell().Text("Product");
+                                header.Cell().AlignRight().Text("Price");
+                            });
+
+                            foreach (var lineItem in lineItems)
+                            {
+                                table.Cell().Text(lineItem.Index.ToString());
+                                table.Cell().Text(lineItem.Name);
+                                table.Cell().Text($"${lineItem.Price}");
+                            }
+                        });
+                });
+            }).GeneratePdf("invoice.pdf");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -29,5 +84,45 @@ namespace LearnQuestPDF.Controllers
             })
             .ToArray();
         }
+        private void QuickStart()
+        {
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                    .Text("Hello PDF!")
+                    .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    page.Content()
+                    .PaddingVertical(1, Unit.Centimetre)
+                    .Column(x =>
+                    {
+                        x.Spacing(20);
+                        x.Item().Text(Placeholders.LoremIpsum());
+                        x.Item().Image(Placeholders.Image(200, 100));
+                    });
+
+                    page.Footer()
+                    .AlignCenter()
+                    .Text(x =>
+                    {
+                        x.Span("Page ");
+                        x.CurrentPageNumber();
+                    });
+                });
+            }).GeneratePdf("Hello.pdf");
+        }
+    }
+    public class LineItem
+    {
+        public int Index { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Price { get; set; } = string.Empty;
     }
 }
