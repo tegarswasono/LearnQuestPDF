944e0850bd69c5fbb061e0faad91b790deebae81
tegarswasono <tegar.s@weefer.co.id>
2024-05-13 18:16:43 +0700

    mantab

---
+++ LearnQuestPDF/Controllers/LearnController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace LearnQuestPDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LearnController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            // code in your main method
+            QuestPDF.Settings.License = LicenseType.Community;
+
+            var tmp = template1();
+            
+            var result = new FileContentResult(tmp, "application/pdf");
+            result.FileDownloadName = "test.pdf";
+            return result;
+        }
+        private byte[] template1()
+        {
+            var tmp = Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                        .Text("Hello PDF!")
+                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    page.Content()
+                        .PaddingVertical(1, Unit.Centimetre)
+                        .Column(x =>
+                        {
+                            x.Spacing(20);
+
+                            x.Item().Text(Placeholders.LoremIpsum());
+                            x.Item().Image(Placeholders.Image(200, 100));
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(x =>
+                        {
+                            x.Span("Page ");
+                            x.CurrentPageNumber();
+                        });
+                });
+            })
+            .GeneratePdf();
+            return tmp;
+        }
+    }
+}
